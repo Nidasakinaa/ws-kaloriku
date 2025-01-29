f895bcc5d6cc0964ef38a03ef9fb5fb7612d7343
nida sakinah aulia <nidasakinaaulia@gmail.com>
2025-01-29 22:22:47 +0700

    Update delete user route to include 'user' prefix for consistency

--- url/url.go
+++ url/url.go
@@ -31,7 +31,7 @@ func Web(page *fiber.App) {
 	page.Get("/user/:id", controller.GetUserID)
 	page.Post("/insert", controller.InsertDataUser)
 	page.Put("/update/:id", controller.UpdateDataUser)
-	page.Delete("/delete/:id", controller.DeleteUserByID)
+	page.Delete("/user/delete/:id", controller.DeleteUserByID)
 	page.Post("/registeruser", handler.Register)
 	page.Post("/login", handler.Login)
 	page.Post("/loginCust", handler.CustomerLogin)
